9f7ebe9ff2ea1b6991ed6648a8497e79e4efa3a3
metildav <jmetilda@in.ibm.com>
2024-07-24 21:25:08 +0530

    commit from gitstf

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("Browser Automation");
+		System.out.println("Web Automation");
+	}
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+
+}
